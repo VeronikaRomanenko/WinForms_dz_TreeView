74db4ef973822b87d786cd2bfdb10964ee732ee8
VeronikaRomanenko <maksymbondarenko@gmail.com>
2019-05-07 20:45:58 +0300

    Commit1

---
+++ WinForms_dz_TreeView/WinForms_dz_TreeView/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WinForms_dz_TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbAdres = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.lsbSoderschimoe = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txbAdres
+            // 
+            this.txbAdres.Location = new System.Drawing.Point(13, 13);
+            this.txbAdres.Name = "txbAdres";
+            this.txbAdres.Size = new System.Drawing.Size(384, 20);
+            this.txbAdres.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 39);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(384, 160);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeSelect);
+            // 
+            // lsbSoderschimoe
+            // 
+            this.lsbSoderschimoe.FormattingEnabled = true;
+            this.lsbSoderschimoe.Location = new System.Drawing.Point(12, 205);
+            this.lsbSoderschimoe.Name = "lsbSoderschimoe";
+            this.lsbSoderschimoe.Size = new System.Drawing.Size(385, 173);
+            this.lsbSoderschimoe.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 389);
+            this.Controls.Add(this.lsbSoderschimoe);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.txbAdres);
+            this.Name = "Form1";
+            this.Text = "Проводник";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbAdres;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListBox lsbSoderschimoe;
+    }
+}
+
---
+++ WinForms_dz_TreeView/WinForms_dz_TreeView/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms_dz_TreeView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FillDriveNodes();
+        }
+
+        private void FillDriveNodes()
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                TreeNode driveNode = new TreeNode(drive.Name);
+                treeView1.Nodes.Add(driveNode);
+            }
+        }
+
+        private void treeView1_BeforeSelect(object sender, TreeViewCancelEventArgs e)
+        {
+            string[] dirs = Directory.GetDirectories(e.Node.FullPath);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                TreeNode driveNode = new TreeNode(new DirectoryInfo(dirs[i]).Name);
+                e.Node.Nodes.Add(driveNode);
+            }
+
+            txbAdres.Text = e.Node.FullPath;
+        }
+        //string[] dirs = Directory.GetDirectories(treeView1.SelectedNode.FullPath);
+        //    for (int i = 0; i<dirs.Length; i++)
+        //    {
+        //        lsbSoderschimoe.Controls.Add(new Control(new DirectoryInfo(dirs[i]).Name));
+        //    }
+    }
+}
